f6792fe1da8df74a310dfa199debf00e20c80989
pranjulknit <69967499+pranjulknit@users.noreply.github.com>
2020-10-14 17:11:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<title>Pranjul Chaurasiya PHP</title>
+<h1>Pranjul Chaurasiya PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Pranjul Chaurasiya\" is ".hash('sha256', 'Pranjul Chaurasiya');
+?></p>
+<pre>ASCII ART:
+    *****************
+    ******************
+    ***             ***
+    ***	            ****
+    ***             ***
+    ******************
+    ****************
+	***
+    ***
+    ***                                           * 
+    ***                                          * * 
+    ***                                         * * * 
+    ***                                        * * * * 
+    ***                                       * * * * *
+                                              
+         
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
